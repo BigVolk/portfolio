d4d44217b06e614721d0704d403fd2d5173b238f
Gabriel Bialik <gab2005345@gmail.com>
2024-11-23 13:44:10 +0200

    mess

--- gabriels-portfolio/src/App.jsx
+++ gabriels-portfolio/src/App.jsx
@@ -1,81 +1,27 @@
 import FrontPage from "./pages/FrontPage"
+import {Parallax, ParallaxLayer} from '@react-spring/parallax'
+import meHappy from './images/meHappy.jpg'
 
 function App() {
 
   return (
     <>
       <FrontPage></FrontPage>
-      random shit in APP.JS
-      <br/>
-      shit
-      <br/>
-      shit
-      <br/>
-      shit
-      <br/>
-      shit
-      <br/>
-      shit
-      <br/>
-      shit
-      <br/>
-      shit
-      <br/>
-      shit
-      <br/>
-      shit
-      <br/>
-      shit
-      <br/>
-      shit
-      <br/>
-      shit<br/>
-      shit<br/>
-      shit<br/>
-      shit<br/>
-      shit<br/>
-      shit<br/>
-      shit<br/>
-      shit<br/>
-      shit<br/>
-      shit<br/>
-      shit<br/>
-      shit<br/>
-      shit<br/>
-      shit<br/>
-      shit<br/>
-      shit<br/>
-      shit<br/>
-      shit<br/>
-      shit<br/>
-      shit<br/>
-      shit<br/>
-      shit<br/>
-      shit<br/>
-      shit<br/>
-      shit<br/>
-      shit<br/>
-      shit<br/>
-      shit<br/>
-      shit<br/>
-      shit<br/>
-      shit<br/>
-      shit<br/>
-      shit<br/>
-      shit<br/>
-      shit<br/>
-      shit<br/>
-      shit<br/>
-      shit<br/>
-      shit<br/>
-      shit<br/>
-      shit<br/>
-      shit<br/>
-      shit<br/>
-      shit<br/>
-      shit
+      <Parallax pages = {3}>
+        <ParallaxLayer
+        offset={0}
+        factor={2}
+        style={{
+          backgroundImage: `url(${meHappy})`,
+          backgroundSize: 'cover',
+          }}>
+        </ParallaxLayer>
+        <ParallaxLayer offset={2}>
+          <h1>test</h1>
+        </ParallaxLayer>
+      </Parallax>
     </>
-    
+
   )
 }
 
