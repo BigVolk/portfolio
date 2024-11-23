b351e2c3214ad739f751ce83e9b7ef2c0311611c
Gabriel Bialik <gab2005345@gmail.com>
2024-11-23 16:54:57 +0200

    Cool site

--- gabriels-portfolio/src/App.jsx
+++ gabriels-portfolio/src/App.jsx
@@ -5,38 +5,50 @@ import meHappy from './images/meHappy.jpg'
 
 function App() {
 
+  const borderSpread = "18px"
   return (
     <div>
       <FrontPage></FrontPage>
-        <Parallax pages={3}
-          style={{ overflowX: 'hidden' }}
-          >
-          <ParallaxLayer
-            speed={0.5}
-            offset={0}
-            factor={1.5}>
-            <Box sx={{
-              backgroundPosition: 'center',
-              backgroundImage: `url(${meHappy})`,
-              backgroundRepeat: 'no-repeat',
-              backgroundSize: 'auto 100%',
+      <Parallax pages={3}
+        style={{ overflowX: 'hidden' }}
+      >
+        <ParallaxLayer
+          speed={0.5}
+          offset={0}
+          factor={1.8}>
+          <Box sx={{
+            backgroundPosition: 'center',
+            backgroundImage: `url(${meHappy})`,
+            backgroundRepeat: 'no-repeat',
+            backgroundSize: 'auto 100%',
+            width: '100%',
+            height: '100%',
+          }} />
+        </ParallaxLayer>
+        <ParallaxLayer offset={1}>
+          <Box
+          sx={
+            {
+              height: `100%`,
               width: '100%',
-              height: '100%',
-            }} />
-          </ParallaxLayer>
-          <ParallaxLayer offset={1}>
+              display: 'flex',
+              alignContent: 'center',
+              alignItems: 'center'
+            }
+          }>
             <Box
-            sx={{
-              boxShadow: '-2px 0px 18px 20px orange', 
-              width: '100%',
-              height: '100%',
-              background: 'orange',
-            }}>
+              sx={{
+                boxShadow: `-2px 0px ${borderSpread} 20px orange`,
+                height: `calc(100% - ${borderSpread} - ${borderSpread} - 30px)`,
+                width: '100%',
+                background: 'orange',
+              }}>
 
             </Box>
-          </ParallaxLayer>
-        </Parallax>
-      </div>
+          </Box>
+        </ParallaxLayer>
+      </Parallax>
+    </div>
   )
 }
 
